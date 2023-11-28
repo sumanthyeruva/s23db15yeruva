832cee475ae90eed156812f307c8450c4adb8a03
sumanthyeruva <yeruva.sumanth07@gmail.com>
2023-11-28 14:13:11 -0600

    created login and register files

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var Employee = require('./models/Employee')
 
 var indexRouter = require('./routes/index');
@@ -23,6 +25,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
+  app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());    
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/resource.js
+++ routes/resource.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+var passport = require('passport');
+var Account = require('../models/account');
 // Require controller modules.
 var api_controller = require("../controllers/api");
 var Employee_controller = require("../controllers/Employee");
@@ -17,4 +19,23 @@ router.put("/Employee/:id", Employee_controller.Employee_update_put);
  router.get("/Employee/:id", Employee_controller.Employee_detail);
 // // GET request for list of all Employee items.
  router.get("/Employee", Employee_controller.Employee_list);
+
+ router.get('/login', function(req, res) {
+    res.render('login', { title: 'Employee App Login', user : req.user });
+    });
+    router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
+    router.get('/logout', function(req, res) {
+    req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+    });
+    });
+    router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+    });
+    router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+    });
 module.exports = router;
