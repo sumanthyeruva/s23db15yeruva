b14c672fcfe1483a07ce0f722dbe3b83efc55626
sumanthyeruva <yeruva.sumanth07@gmail.com>
2023-11-29 22:14:23 -0600

    added part 4 and 5

--- models/Employee.js
+++ models/Employee.js
@@ -1,8 +1,8 @@
  const mongoose = require("mongoose")
  const EmployeeSchema = mongoose.Schema({
-     Employee_fname: String,
-     Employee_lname: String,
-     Employee_id   : Number
+     Employee_fname: {type: String,minlength: 3 },
+     Employee_lname: {type: String,required:true},
+     Employee_id   : {type: Number,required:true}
  })
  module.exports = mongoose.model("Employee",
  EmployeeSchema)
--- routes/Employee.js
+++ routes/Employee.js
@@ -1,18 +1,23 @@
 var express = require('express');
 const Employee_controlers= require('../controllers/Employee');
 var router = express.Router();
-
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+}
 router.get('/', Employee_controlers.Employee_view_all_Page );
 // GET request for one Employee
 router.get('/Employee/:id', Employee_controlers.Employee_detail);
 /* GET detail Employee page */
 router.get('/detail', Employee_controlers.Employee_view_one_Page);
 /* GET create Employee page */
-router.get('/create', Employee_controlers.Employee_create_Page);
+router.get('/create',secured, Employee_controlers.Employee_create_Page);
 /* GET create update page */
-router.get('/update', Employee_controlers.Employee_update_Page);
+router.get('/update',secured, Employee_controlers.Employee_update_Page);
 /* GET delete Employee page */
-router.get('/delete', Employee_controlers.Employee_delete_Page);
+router.get('/delete',secured, Employee_controlers.Employee_delete_Page);
 
 
 module.exports = router;
